d1bc7538e92da078453170ac672c56c49faa7770
Deepak Raja <12306deepakraja@gmail.com>
2023-09-23 15:47:40 +0530

    Added the fetching

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Allow requests from the specified frontend origin
+app.use(cors());
+const dbURI = "mongodb+srv://Deepakraja:Barryallen03@cluster0.3atagu3.mongodb.net/Cars";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
+
+// Parse incoming JSON data
+app.use(express.json());
+
+require("../backend/models/card")
+const User = mongoose.model("CarInfo");
+app.post("/post", async(req,res) => {
+    const {companyname,modelname,year,amount} = req.body;
+    try {
+        await User.create({
+            companyname,
+            modelname,
+            year,
+            amount,
+        });
+        res.send({status:"ok"});
+    } catch (error) {
+        res.send({status:"error"})
+    }
+});
+
+// Define a route to fetch car information
+app.get("/fetchCars", async (req, res) => {
+  try {
+    const carInfo = await User.find(); // Assuming User is your Mongoose model
+    res.json(carInfo); // Send the car information as JSON response
+  } catch (error) {
+    console.error("Error fetching car info:", error);
+    res.status(500).json({ error: "Internal Server Error" }); // Handle errors gracefully
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
--- src/App.js
+++ src/App.js
@@ -1,97 +1,139 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
-function DisplayValues(props) {
-  return (
-    <div className=' mt-8'>
-      <div className='bg-white w-80 pt-6 ml-6 pb-6 rounded-3xl'>
-        <h2 className='text-center text-2xl'>Details</h2>
-        <div className='pl-6 pt-4 space-y-2'>
-          <div className='flex items-start'>
-            <span className='w-32 font-bold'>Name:</span>
-            <span className='flex-1 truncate'>{props.name}</span>
-          </div>
-          <div className='flex items-start'>
-            <span className='w-32 font-bold'>Model:</span>
-            <span className='flex-1 truncate'>{props.desc}</span>
-          </div>
-          <div className='flex items-start'>
-            <span className='w-32 font-bold'>Year:</span>
-            <span className='flex-1 truncate'>{props.year}</span>
-          </div>
-          <div className='flex items-start'>
-            <span className='w-32 font-bold'>Price:</span>
-            <span className='flex-1 truncate'>{props.amount}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+// function DisplayValues(props) {
+//   return (
+//     <div className=' mt-8'>
+//       <div className='bg-white w-80 pt-6 ml-6 pb-6 rounded-3xl'>
+//         <h2 className='text-center text-2xl'>Details</h2>
+//         <div className='pl-6 pt-4 space-y-2'>
+//           <div className='flex items-start'>
+//             <span className='w-32 font-bold'>Name:</span>
+//             <span className='flex-1 truncate'>{props.name}</span>
+//           </div>
+//           <div className='flex items-start'>
+//             <span className='w-32 font-bold'>Model:</span>
+//             <span className='flex-1 truncate'>{props.desc}</span>
+//           </div>
+//           <div className='flex items-start'>
+//             <span className='w-32 font-bold'>Year:</span>
+//             <span className='flex-1 truncate'>{props.year}</span>
+//           </div>
+//           <div className='flex items-start'>
+//             <span className='w-32 font-bold'>Price:</span>
+//             <span className='flex-1 truncate'>{props.amount}</span>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 
 function App() {
-  const [name, setName] = useState('');
-  const [desc, setDesc] = useState('');
+  const [companyname, setCompnayName] = useState('');
+  const [modelname, setModelName] = useState('');
   const [year, setYear] = useState('');
   const [amount, setAmount] = useState('');
-  const [posts, setPosts] = useState([]);
+  const [carInfoList, setCarInfoList] = useState([]);
+
 
   function post() {
-    const _name = document.getElementById('name').value;
-    const _desc = document.getElementById('desc').value;
+    const _companyname = document.getElementById('name').value;
+    const _modelname = document.getElementById('desc').value;
     const _year = document.getElementById('year').value;
     const _amount = document.getElementById('amount').value;
 
-    if(!_name |!_desc |!_year |!_amount){
+    if(!_companyname |!_modelname |!_year |!_amount){
       alert("Please fill in all fields before posting.")
     }
     else{
-    const newPost = {
-      name: _name,
-      desc: _desc,
-      year: _year,
-      amount: _amount
-    };
-    setPosts([...posts, newPost]);
-    setName('');
-    setDesc('');
+    setCompnayName('');
+    setModelName('');
     setYear('');
     setAmount('');
   }
+  fetch("http://localhost:3000/post", {
+      method:"POST",
+      crossDomain:true,
+      headers:{
+        "Content-Type":"application/json",
+        Accept:"application/json",
+        "Access-Control-Allow-Origin":"*"
+      },
+      body:JSON.stringify({
+        companyname,
+        modelname,
+        year,
+        amount,
+      }),
+      }).then((res)=>res.json())
+        .then((data) => {
+          console.log(data,"userPost");
+        })
   }
-
+  function fetchCarInfo() {
+    fetch("http://localhost:3000/fetchCars")
+      .then((res) => res.json())
+      .then((data) => {
+        setCarInfoList(data); // Update the state with the retrieved car info
+      })
+      .catch((error) => {
+        console.error("Error fetching car info:", error);
+      });
+  }
+  useEffect(() => {
+    fetchCarInfo();
+  }, []); // The empty array [] means this effect runs once when the component mounts
+  
+  
   return (
     <div className='bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 bg-cover pb-6'>
       <h1 className=' text-6xl text-center pt-10 text-gray-800 underline'>CarBuyer</h1>
       <h1 className="text-3xl text-center mt-5 text-gray-800">Car Details</h1>
-        <div className=' ml-96'> 
-          <input type="text" placeholder='Company Name' id='name' value={name} onChange={(e) => setName(e.target.value)} 
-          className=' text-center w-96 h-9 mt-10 rounded-2xl ml-16 pl-16 pr-16' required /><br />
-          <input type="text" placeholder='Model Name' id='desc' value={desc} onChange={(e) => setDesc(e.target.value)} 
-          className=' text-center w-96 h-9 mt-10 rounded-2xl ml-16 pl-16 pr-16' required /><br />
+        <center className=''> 
+          <input type="text" placeholder='Company Name' id='name' value={companyname} onChange={(e) => setCompnayName(e.target.value)} 
+          className=' text-center w-96 h-9 mt-10 rounded-2xl flex justify-center' required /><br />
+          <input type="text" placeholder='Model Name' id='desc' value={modelname} onChange={(e) => setModelName(e.target.value)} 
+          className=' text-center w-96 h-9 mt-10 rounded-2xl flex justify-center' required /><br />
           <input type="text" placeholder='Year' id='year' value={year} onChange={(e) => setYear(e.target.value)} 
-          className=' text-center w-96 h-9 mt-10 rounded-2xl ml-16 pl-16 pr-16' required /><br />
+          className=' text-center w-96 h-9 mt-10 rounded-2xl flex justify-center' required /><br />
           <input type="text" placeholder='Price' id='amount' value={amount} onChange={(e) => setAmount(e.target.value)} 
-          className=' text-center w-96 h-9 mt-10 rounded-2xl ml-16 pl-16 pr-16' required /><br />
-        </div>
-        <div className=' ml-96'>
-          <input type='button' value='Post' onClick={post} className=' text-white ml-[200px] mt-6 bg-red-600 w-28 h-8 rounded-2xl border-black hover:bg-fuchsia-600 cursor-pointer align-middle text-center' /><br /><br />
-        </div>
+          className=' text-center w-96 h-9 mt-10 rounded-2xl flex justify-center' required /><br />
+        </center>
+        <center className=''>
+          <input type='button' value='Post' onClick={post} className=' text-white mt-6 bg-red-600 w-28 h-8 rounded-2xl border-black hover:bg-fuchsia-600 cursor-pointer align-middle text-center' /><br /><br />
+        </center>
         <div>
-          <h1 className=' text-6xl text-center pt-10 text-gray-800 underline pb-5'>Buy Cars</h1>
+          <center className=' text-6xl text-center pt-10 text-gray-800 underline pb-5'>
+            Buy Cars
+          </center>
         </div>
-        <div className="mx-20">
-          {posts.length === 0 && (
-            <p className="text-center pt-6 text-2xl text-black">No cars available at the moment</p>
-          )}
-          <div className='grid grid-cols-3'>
-            {posts.map((post, index) => (
-              <DisplayValues key={index} name={post.name} desc={post.desc} year={post.year} amount={post.amount} />
-            ))}
+        <div className='mt-8 grid lg:grid-cols-3 gap-7 sm:grid-cols-1'>
+          {carInfoList.map((carInfo) => (
+        <div key={carInfo._id}>
+          <div className="bg-white w-80 pt-6 ml-6 pb-6 rounded-3xl">
+            <h2 className='text-center text-2xl'>Details</h2>
+            <div className='flex items-start mt-1'>
+              <span className='w-32 font-bold ml-5 mt-2'>Company:</span>
+              <span className='flex-1 truncate mt-2'>{carInfo.companyname}</span>
+            </div>
+            <div className='flex items-start'>
+              <span className='w-32 font-bold ml-5 mt-2'>Model:</span>
+              <span className='flex-1 truncate mt-2'>{carInfo.modelname}</span>
+            </div>
+            <div className='flex items-start'>
+              <span className='w-32 font-bold ml-5 mt-2'>Year:</span>
+              <span className='flex-1 truncate mt-2'>{carInfo.year}</span>
+            </div>
+            <div className='flex items-start'>
+              <span className='w-32 font-bold ml-5 mt-2'>Price:</span>
+              <span className='flex-1 truncate mt-2'>{carInfo.amount}</span>
+            </div>
           </div>
+        </div>
+      ))}
       </div>
-
     </div>
   );
 }
